95d274a9f74e7866ca316d79bdda2dd2c5deaf0e
HeCCXX <985701799@qq.com>
2019-12-04 11:21:44 +0800

    公共页面抽取、链接高亮、新增页面的实验

--- src/main/java/com/hcx/springbootweb/controller/HelloController.java
+++ src/main/java/com/hcx/springbootweb/controller/HelloController.java
@@ -1,11 +1,18 @@
 package com.hcx.springbootweb.controller;
 
+import com.hcx.springbootweb.dao.DepartmentDao;
+import com.hcx.springbootweb.dao.EmployeeDao;
+import com.hcx.springbootweb.entities.Department;
+import com.hcx.springbootweb.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collection;
 import java.util.Map;
 
 /**
@@ -17,6 +24,12 @@ import java.util.Map;
  **/
 @Controller
 public class HelloController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
     @ResponseBody
     @RequestMapping("/hcx")
@@ -30,8 +43,28 @@ public class HelloController {
             session.setAttribute("loginUser",username);
             return "redirect:/main.html";
         }else {
-            map.put("msg","用户名面错误");
+            map.put("msg","用户名密码错误");
             return "login";
         }
+    }
+
+    @GetMapping("/emps")
+    public String showEmps(Model model){
+        Collection<Employee> emps = employeeDao.getAll();
+        model.addAttribute("emps",emps);
+
+        return "practice/list";
+    }
+
+//    @PostMapping("/addUser")
+//    public String addEmps(){
+//        return "practice/add";
+//    }
+
+    @GetMapping("/emp")
+    public String addEmps(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return  "practice/add";
     }
 }
